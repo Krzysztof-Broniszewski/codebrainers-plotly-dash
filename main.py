48f9ea4af800ce8bcf653e0f1a282526df30720b
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2025-02-04 18:19:32 +0100

    add-dash

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
+import dash
+from dash import html
 
 URL = "https://api.nbp.pl/api/cenyzlota/2024-01-01/2024-12-31/?format=json"
 
 response = requests.get(URL)
 data = response.json()
 
-print(data)
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1("Witaj w Dash!"),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
