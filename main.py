9dc25b70ae5f2e782aea09e6b60134a41de6bbd8
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2025-02-04 18:14:16 +0100

    add-url-response

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import requests
-import pandas as pd
-from io import StringIO
 
-# URL pliku CSV
-url = "https://api.dane.gov.pl/1.4/resources/56169,zdarzenia-w-ruchu-drogowych-w-2023-r-sprawcy-pojazd-uczestnika?lang=pl"
+URL = "https://api.nbp.pl/api/cenyzlota/2024-01-01/2024-12-31/?format=json"
 
-# Pobranie danych
-response = requests.get(url)
+response = requests.get(URL)
+data = response.json()
 
-# Sprawdzenie, czy pobrano poprawnie
-if response.status_code == 200:
-    csv_data = StringIO(response.text)  # Konwersja do formatu pliku tekstowego
-    df = pd.read_csv(csv_data, sep=";")  # Wczytanie CSV do pandas
-    print(df.head())  # Podgląd pierwszych wierszy
-else:
-    print(f"Błąd pobierania: {response.status_code}")
-
+print(data)
