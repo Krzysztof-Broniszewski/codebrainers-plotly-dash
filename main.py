b13b42995bebb68db5556ce0fa43745acd7dcd75
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2025-02-04 19:28:13 +0100

    options-as-list

--- main.py
+++ main.py
@@ -35,9 +35,11 @@ app.layout = html.Div(children=[
     dcc.RadioItems(
         id='year-selector',
         options=[
-            {'label': '2024', 'value': '2024'}
+            {'label': '2023', 'value': '2023'},
+            {'label': '2024', 'value': '2024'},
+            {'label': 'Oba lata', 'value': 'all'}
         ],
-        value='2024'
+        value='all'
     ),
     html.Div(children=[
         dcc.Graph(id='gold-price-graph')
