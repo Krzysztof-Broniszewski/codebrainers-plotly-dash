2adf4b17e28304857d3e57c9d2c945e55c18a114
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2025-02-04 18:23:41 +0100

    add-buttons-dcc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import dash
-from dash import html
+from dash import html, dcc
 
 URL = "https://api.nbp.pl/api/cenyzlota/2024-01-01/2024-12-31/?format=json"
 
@@ -9,8 +9,17 @@ data = response.json()
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H1("Witaj w Dash!"),
+app.layout = html.Div([
+    html.H1("Podstawy Dash"),
+    html.P("Przykład wykorzystania komponentów HTML i interaktywnych."),
+    html.Ul([
+        html.Li("Pierwszy element listy"),
+        html.Li("Drugi element listy"),
+        html.Li("Trzeci element listy")
+    ]),
+    html.Button("Kliknij mnie", id="example-button"),
+    dcc.Input(id="example-input", type="text", placeholder="Wpisz coś..."),
+    dcc.Graph(id="example-graph")
 ])
 
 if __name__ == '__main__':
